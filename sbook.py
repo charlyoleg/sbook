1cc302ce6be6f61b69cdc0afdd2143162b48b2ee
charli <charlyoleg@fabfolk.com>
2019-07-10 22:38:06 +0200

    complete removing of sw_grep

--- sbook.py
+++ sbook.py
@@ -28,8 +28,6 @@ sb_parser = argparse.ArgumentParser(description=sb_usage)
 sb_parser.add_argument('key_words', nargs='+', help='the searched words.')
 sb_parser.add_argument('-d', '--directory', action='append', dest='sw_directory',
                       help="Path to the input file used as directory. Directory file format: one entry is a paragraph. Paragraph are marked off with one empty line. \# is comment and won't be displayed.")
-sb_parser.add_argument('-g', '--grep', action='append', dest='sw_grep',
-                      help="words used to select the displayed paragraph.")
 sb_parser.add_argument('-v', '--grep_v', action='append', dest='sw_grep_v',
                       help="words used to unselect the displayed paragraph.")
 sb_parser.add_argument('-a', '--append_filename', action='store_true', dest='sw_append_filename', default=False,
@@ -42,11 +40,8 @@ if sb_args.sw_directory is None:
   sys.exit(2)
 
 #print "dbg220: sb_args:", sb_args
-#print "dbg221: sb_args.sw_grep:", sb_args.sw_grep
 key_word_select = sb_args.key_words
 key_word_unselect = []
-if not sb_args.sw_grep is None:
-  key_word_select.extend(sb_args.sw_grep)
 if not sb_args.sw_grep_v is None:
   key_word_unselect.extend(sb_args.sw_grep_v)
 if len(key_word_select)<1:
