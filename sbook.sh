cace878b50f97ab933cf0fcbfd9cc6f8e0298979
charli <charlyoleg@fabfolk.com>
2019-07-10 22:30:05 +0200

    modernizing the scripts

--- sbook.sh
+++ sbook.sh
@@ -2,6 +2,8 @@
 
 # wrapper script for the python script sbook.py
 
+cd $(dirname $0)
+
 # just use the python script like this
 python sbook.py -d my_sbook_data_contacts.txt -d my_sbook_data_passwords.txt $@
 
